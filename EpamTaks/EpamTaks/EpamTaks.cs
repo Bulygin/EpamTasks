46b2467f03055d55fe582a2f24d8c5ca0c8e6fe8
Bulygin <jbulygin@yandex.ru>
2021-03-25 17:39:46 +0300

    fourth task

--- EpamTaks/EpamTaks/EpamTaks.cs
+++ EpamTaks/EpamTaks/EpamTaks.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EpamTaks {
   public class EpamTaks {
     static void Main(string[] args) {
+    }
+    public static string[] getWordsOnLetter(string text, char letter) {
+      List<string> result = new List<string>();
+      foreach (string word in text.Split(' ')) {
+        if (char.ToLower(word[0]) == char.ToLower(letter)) {
+          result.Add(word);
+        }
+      }
+      return result.ToArray();
     }
   }
 }
--- EpamTests/EpamTests.cs
+++ EpamTests/EpamTests.cs
@@ -4,6 +4,11 @@ using EpamTaks;
 namespace EpamTaks.Tests {
   [TestClass()]
   public class EpamTests {
+    [TestMethod()]
+    public void getWordsOnLetterTest() {
+      Assert.AreEqual(EpamTaks.getWordsOnLetter("Test text with different words", 't')[0], "Test");
+      Assert.AreEqual(EpamTaks.getWordsOnLetter("Test text with different words", 'T')[0], "Test");
+    }
   }
 }
 
