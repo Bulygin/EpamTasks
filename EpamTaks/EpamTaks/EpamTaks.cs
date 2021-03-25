fbdf837d961ec5e39d732b91c98eb3419dcba7fe
Bulygin <jbulygin@yandex.ru>
2021-03-25 15:44:57 +0300

    empty projects

---
+++ EpamTaks/EpamTaks/EpamTaks.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EpamTaks {
+  public class EpamTaks {
+    static void Main(string[] args) {
+    }
+  }
+}
---
+++ EpamTests/EpamTests/EpamTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EpamTaks;
+
+namespace EpamTaks.Tests {
+  [TestClass()]
+  public class EpamTests {
+  }
+}
+
