56cfa75afc187e890abb5cffc8bf37798e28ee55
Bulygin <jbulygin@yandex.ru>
2021-03-25 16:03:22 +0300

    first task

--- EpamTaks/EpamTaks/EpamTaks.cs
+++ EpamTaks/EpamTaks/EpamTaks.cs
@@ -4,5 +4,9 @@ namespace EpamTaks {
   public class EpamTaks {
     static void Main(string[] args) {
     }
+    public static ulong getFactorial(ulong num) {
+      if (num == 0) return 1;
+      return getFactorial(num - 1) * num;
+    }
   }
 }
--- EpamTests/EpamTests.cs
+++ EpamTests/EpamTests.cs
@@ -4,6 +4,13 @@ using EpamTaks;
 namespace EpamTaks.Tests {
   [TestClass()]
   public class EpamTests {
+    [TestMethod()]
+    public void getFactorialTest() {
+      Assert.AreEqual(EpamTaks.getFactorial(0), (ulong)1);
+      Assert.AreEqual(EpamTaks.getFactorial(1), (ulong)1);
+      Assert.AreEqual(EpamTaks.getFactorial(5), (ulong)120);
+      Assert.AreEqual(EpamTaks.getFactorial(54), 10519282829630636032);
+    }
   }
 }
 
